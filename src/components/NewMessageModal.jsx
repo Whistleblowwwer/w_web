a220c6d48efd9d2ee675d13703d31652bf5be477
Jorge de Leon <jorge.deleon@lernit.com.mx>
2023-12-23 22:12:35 -0600

    crear chat nuevo desde icono de nuevo mensaje

---
+++ src/components/NewMessageModal.jsx
@@ -0,0 +1,57 @@
+import mas from "../assets/Group 99.svg";
+import proSet from "../assets/Image-40.png";
+import UserListAutoComplete from "./UserListAutoComplete";
+
+function NewMessageModal(props) {
+  return (
+    <div className="fixed top-0 left-0 w-full h-screen flex items-center justify-center z-50">
+      <div className="absolute top-0 left-0 w-full h-full bg-black opacity-70"></div>
+      <div
+        className={`z-50 bg-[#FFF] p-4 shadow-md rounded-[20px] min-h-auto md:w-2/4 w-[80%]`}
+      >
+        <div className="flex flex-col">
+          <div>
+            <p className="text-lg">Nuevo mensaje</p>
+          </div>
+          <div className="flex justify-end mr-2">
+            <button
+              onClick={() => props.handleCloseNewMessageModal()}
+              className={`hover:text-gray-500 cursor-pointer`}
+            >
+              <i className="fas fa-times text-2xl"></i>
+            </button>
+          </div>
+
+          <div className="flex sm:flex-row flex-col items-center justify-between w-full mt-5 gap-2">
+            <div className="flex gap-2 w-full">
+              <div
+                onClick={props.handleSearchCompanyClick}
+                className="relative w-full"
+              >
+                {/* reutilizando para nuevo mensaje */}
+                <UserListAutoComplete
+                  suggestions={props.suggestions}
+                  usersSearchQuery={props.usersSearchQuery}
+                  setUsersSearchQuery={props.setUsersSearchQuery}
+                  setNewMessageUser={props.setNewMessageUser}
+                  newMessageUser={props.newMessageUser}
+                />
+              </div>
+            </div>
+          </div>
+
+          <div className="flex flex-col sm:flex-row items-center justify-between w-full mt-5">
+            <button
+              onClick={props.handleNewConversation}
+              className={` bg-neutral-900 rounded-[20.50px] border px-4 py-2 sm:mt-0 mt-5`}
+            >
+              <p className={`text-stone-50`}>Siguiente</p>
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default NewMessageModal;
---
+++ src/components/UserListAutoComplete.jsx
@@ -0,0 +1,69 @@
+import { useState, useEffect, onSelect } from "react";
+import classNames from "classnames";
+
+function CompanyAutocomplete(props) {
+  const [query, setQuery] = useState("");
+  const [filteredSuggestions, setFilteredSuggestions] = useState([]);
+  const [isDropdownOpen, setDropdownOpen] = useState(false);
+
+  const suggestions = props.suggestions;
+
+  useEffect(() => {
+    setFilteredSuggestions(
+      props?.suggestions.filter((suggestion) =>
+        suggestion?.name
+          .toLowerCase()
+          .includes(props.usersSearchQuery.toLowerCase())
+      )
+    );
+    if (props.usersSearchQuery.length > 0) {
+    }
+  }, [props.usersSearchQuery, props.suggestions]);
+
+  const handleInputChange = (event) => {
+    props.setUsersSearchQuery(event.target.value);
+    setDropdownOpen(true);
+  };
+
+  const handleSelectSuggestion = (suggestion) => {
+    props.setUsersSearchQuery(`${suggestion?.name} ${suggestion?.last_name}`);
+    props.setNewMessageUser({
+      ...props.newMessageUser, // Preserve existing key-value pairs
+      name: `${suggestion?.name} ${suggestion?.last_name}`,
+      userId: suggestion._id_user,
+    });
+    setDropdownOpen(false);
+  };
+
+  return (
+    <div className="relative w-full">
+      <input
+        type="text"
+        value={props.usersSearchQuery}
+        onChange={handleInputChange}
+        placeholder="Buscar personas"
+        className="w-full p-2 border rounded-md"
+      />
+      {isDropdownOpen && props.usersSearchQuery.length > 0 && (
+        <ul className="absolute z-50 w-full h-full mt-1 bg-white border rounded-md shadow-md">
+          {filteredSuggestions.map((suggestion) => (
+            <li
+              key={suggestion?._id_business}
+              className={classNames(
+                "p-2 cursor-pointer bg-white hover:bg-gray-100",
+                {
+                  "bg-gray-100": props.companySearchQuery === suggestion,
+                }
+              )}
+              onClick={() => handleSelectSuggestion(suggestion)}
+            >
+              {`${suggestion?.name} ${suggestion?.last_name}`}
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+}
+
+export default CompanyAutocomplete;
