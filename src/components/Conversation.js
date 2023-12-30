a415bc45cf7b13be07029e89a567424cc7da420f
Elcale1703 <m.agudeloo@uniandes.edu.co>
2023-12-29 19:50:14 -0500

    Chat response

--- src/components/Conversation.js
+++ src/components/Conversation.js
@@ -1,5 +1,5 @@
 import { React, useRef, useEffect } from "react";
-import sendMessage from "../assets/SendMessage.svg";
+import sendMessage from "../assets/enviar-mensaje-removebg-preview.png";
 
 function Conversation(props) {
   const sender = props.userId;
@@ -19,41 +19,81 @@ function Conversation(props) {
   };
 
   return (
-    <div className="conversation-container">
-      <div className="flex justify-center pt-2 fixed w-[100%] top-0 left-[19%] ">
-        <p className="text-zinc-900 text-xl font-medium leading-[18.62px]">
-          {props?.userName}
-        </p>
-      </div>
-      <div className="messages">
-        {props.messages && props.messages.length > 0 ? (
-          props.messages.map((message, index) => (
-            <div
-              key={index}
-              className={`message ${message._id_sender === props.userId ? "sent" : "received"}`}
-            >
-              {message.content}
-            </div>
-          ))
-        ) : (
-          <p className="flex justify-center text-gray-600	">Iniciar conversacion</p>
+    <div>
+      <div className="conversation-container">
+        <div className="flex justify-center fixed w-[100%] top-0 left-[19%]">
+          <p className="text-zinc-900 text-center p-2 text-xl font-medium leading-[18.62px] h-10 w-[66%] translate-x-[6%]" style={{ backgroundColor: 'rgba(169, 169, 169, 0.06)' }}>
+            {props?.userName}
+          </p>
+        </div>
+        <div className="messages mt-4">
+          {props.messages && props.messages.length > 0 ? (
+            props.messages.map((message, index) => (
+              <div
+                key={index}
+                className={`message ${message._id_sender === props.userId ? "sent" : "received"}`}
+              >
+                {message.content}
+              </div>
+            ))
+          ) : (
+            <p className="flex justify-center text-gray-600	">Iniciar conversacion</p>
 
-        )}
-        <div ref={messagesEndRef}></div>
-        <div className="input-container">
-          <input
-            type="text"
-            placeholder="Type your message..."
-            value={props.message}
-            onChange={(e) => props.setMessage(e.target.value)}
-          />
-          <button className="bg-neutral-900 rounded-[25px]" onClick={() => handleSendMessage()}>
-            <img src={sendMessage} alt="sendmessage" />
-          </button>
+          )}
+          <div ref={messagesEndRef}></div>
+          <div className="input-container">
+            <input
+              type="text"
+              placeholder="Type your message..."
+              value={props.message}
+              onChange={(e) => props.setMessage(e.target.value)} // Agrega esta clase para hacer el input más redondo
+            />
+            <button
+              onClick={() => handleSendMessage()}
+            >
+              <img src={sendMessage} alt="send" className="translate-x-[-60%]" />
+            </button>
+          </div>
+        </div>
+      </div>
+      <div className="conversation-response">
+        <div className="fixed w-[100%] top-[9%] z-10000">
+          <span className="absolute mt-2 ml-2" onClick={props.handleChatVisible}><i class="fa-solid fa-arrow-left"></i></span>
+          <p className="flex justify-center text-zinc-900 text-center p-2 text-xl font-medium leading-[18.62px] h-10 w-[100%] " style={{ backgroundColor: 'rgba(169, 169, 169, 0.06)' }}>
+            {props?.userName}
+          </p>
+        </div>
+        <div className="messages mt-6">
+          {props.messages && props.messages.length > 0 ? (
+            props.messages.map((message, index) => (
+              <div
+                key={index}
+                className={`message ${message._id_sender === props.userId ? "sent" : "received"}`}
+              >
+                {message.content}
+              </div>
+            ))
+          ) : (
+            <p className="flex justify-center text-gray-600	">Iniciar conversacion</p>
+
+          )}
+          <div ref={messagesEndRef}></div>
+          <div className="input-container">
+            <input
+              type="text"
+              placeholder="Type your message..."
+              value={props.message}
+              onChange={(e) => props.setMessage(e.target.value)}
+            />
+            <button
+              onClick={() => handleSendMessage()}
+            >
+              <img src={sendMessage} alt="send" className="" />
+            </button>
+          </div>
         </div>
       </div>
     </div>
-
   );
 }
 
